06989cdfaeae1795d86c29f682978cbb58bad6de
russseaman <russ_seaman@mac.com>
2019-01-24 09:37:05 -0500

    updating class slides...421 assignments

--- SP2019/CSCI421/LectureActivies/Lec1Act1/ThreeSum.java
+++ SP2019/CSCI421/LectureActivies/Lec1Act1/ThreeSum.java
@@ -5,14 +5,20 @@ public class ThreeSum {
     for (int i = 0; i < N; i++)
       for (int j = i+1; j < N; j++)
         for (int k = j+1; k < N; k++)
-          if (a[i] + a[j] + a[k] == 0)
+          if (a[i] + a[j] + a[k] == 0){
+            System.out.println("Triplet Found: " + a[i] + " " + a[j] + " " + a[k] );
             count++;
+            }
     return count;
   }
 
-public static void main(String[] args){
-  In in = new In(args[0]);
-  int[] a = in.readAllInts(-25,-10,-7,-3,2,4,8,10);
-  StdOut.println(count(a));
+  public static void main(String[] args){
+    int arr1[] = {-25, -10, -7, -3, 2, 4, 8, 10};
+    System.out.println("For the array: ");
+    for(int i = 0; i < arr1.length; i++){
+      System.out.print(arr1[i] + " ");
+    }
+    System.out.println();
+    System.out.println("Total triplets found: " + count(arr1));
   }
 }
