8d7902f85f3d41270b1ac99a2acf490f5dd0a49e
Russ Seaman <russ_seaman@mac.com>
2019-01-31 10:21:37 -0500

    updates

---
+++ test.py
@@ -0,0 +1,8 @@
+#playing with hydrogen....works pretty well....need to work on the jupyter notebook stuff
+#having problems with the theme and things showing up
+print("Hello, World!")
+
+x = 2
+y = 5
+
+print("{} + {} = {}".format(x,y,x+y))
