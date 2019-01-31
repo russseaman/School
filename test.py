0f1bc47c17a54219b8a1739fd6d720c8ee0107ef
Russ Seaman <russ_seaman@mac.com>
2019-01-31 10:37:32 -0500

    removing trash...uneeded test file

--- test.py
+++
@@ -1,8 +0,0 @@
-#playing with hydrogen....works pretty well....need to work on the jupyter notebook stuff
-#having problems with the theme and things showing up
-print("Hello, World!")
-
-x = 2
-y = 5
-
-print("{} + {} = {}".format(x,y,x+y))
