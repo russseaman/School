fa071208a75e7178a80cd25d509cc106a1f02b93
russseaman <russ_seaman@mac.com>
2019-02-11 11:32:46 -0500

    updating current state of classes

--- SP2019/MATH315/Homework/Homework1/hw1q3g_pie.R
+++ SP2019/MATH315/Homework/Homework1/hw1q3g_pie.R
@@ -1,10 +1,9 @@
 Tcor = read.csv("C:/Users/russ_/Documents/Git/SCHOOL/SP2019/MATH315/Homework/Homework1/ToyotaCorolla.csv", header=T, sep=",")
 
 Ftable = table(Tcor[8])
-class = unique(Tcor[8])
+class = sort(unique(Tcor[8]))
 
 pct<- round(Ftable/sum(Ftable)*100,1)
 class<-paste(class, pct)
 class<-paste(class,"%",sep="")
 pie(Ftable,labels=class, col=rainbow(length(class)),main="Piechart of Fuel Types for Toyota Corolla")
-
