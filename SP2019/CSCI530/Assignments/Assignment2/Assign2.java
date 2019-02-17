0f233bc1c90fa7c4b10e248271270e698fe60622
russseaman <russ_seaman@mac.com>
2019-02-17 14:09:09 -0500

    current working state for 530 assign 2....pulling file in and parsing all chars indivually....working on anylyzing input.

--- SP2019/CSCI530/Assignments/Assignment2/Assign2.java
+++ SP2019/CSCI530/Assignments/Assignment2/Assign2.java
@@ -1,42 +1,60 @@
 import java.io.*;
 import java.util.*;
+import java.util.Spliterator;
 import java.nio.CharBuffer;
 import java.nio.file.*;
 import java.util.stream.*;
 
 @SuppressWarnings("rawtypes")
 public class Assign2 {
-  //
-  // public static void openFile(String filename) throws IOException {
-  //   File file = new File(filename);
-  //   BufferedReader br = new BufferedReader(new FileReader(file));
-  //
-  //   String st;
-  //   while ((st = br.readLine()) != null)
-  //     System.out.println(st);
-  //
-  //   br.close();
-  // }
+
+  // public static void keywordCheck(String charAt)
+
+  public static void stringParse(String filename) throws FileNotFoundException {
+    List<Character> charList = new ArrayList<Character>();
+    PrintStream ps = new PrintStream(System.out);
+    Scanner s = null;
+    try {
+      s = new Scanner(new BufferedReader(new FileReader(filename)));
+      while (s.hasNext()) {
+        String str = s.next();
+        for (int i = 0; i <= str.length() - 1; i++) {
+          // if (i == 0) {
+          //   System.out.println((i + 1) + "\t" + str.charAt(i));
+          // }
+          charList.add(str.charAt(i));
+          ps.println(charList);
+        }
+      }
+    } finally {
+      if (s != null) {
+        s.close();
+      }
+    }
+
+  }
+
+  public static class keywords {
+    List<String> keywords = Arrays.asList("int", "double", "String");
+  }
+
+  public class operators {
+    List operators = Arrays.asList("=", "(", ")", "+", "-", "*", "/", ",", ";");
+  }
+
+  public class printRes {
+
+  }
 
   public static void main(String[] args) throws IOException {
-    // Scanner sc = new Scanner(System.in);
-    List keywords = Arrays.asList("int", "double", "String");
-    List operators = Arrays.asList("=", "(", ")", "+", "-", "*", "/", ",", ";");
-    // keywords.forEach(n -> System.out.println(n));
-    // operators.forEach(n -> System.out.println(n));
+    Scanner sc = new Scanner(System.in);
 
     String filename = "input.txt";
     // System.out.println("Please enter a filename: ");
-    //
     // String filename = sc.next();
-    // openFile(filename);
+    stringParse(filename);
     // sc.close();
-    //
-    //
-    Stream<String> streamLines = Files.lines(Paths.get(filename));
-    streamLines.forEach(System.out::println);
 
   }
-}
 
-// this is a test
+}
