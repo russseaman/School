014d2950eae66d63a4a89e4dd0bef288cf06aebd
russseaman <russ_seaman@mac.com>
2019-02-17 08:21:19 -0500

    bringing assign2 current with testbed

--- SP2019/CSCI530/Assignments/Assignment2/Assign2.java
+++ SP2019/CSCI530/Assignments/Assignment2/Assign2.java
@@ -1,6 +1,10 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+import java.nio.CharBuffer;
+import java.nio.file.*;
+import java.util.stream.*;
 
+@SuppressWarnings("rawtypes")
 public class Assign2 {
   //
   // public static void openFile(String filename) throws IOException {
@@ -16,18 +20,22 @@ public class Assign2 {
 
   public static void main(String[] args) throws IOException {
     // Scanner sc = new Scanner(System.in);
-    //
+    List keywords = Arrays.asList("int", "double", "String");
+    List operators = Arrays.asList("=", "(", ")", "+", "-", "*", "/", ",", ";");
+    // keywords.forEach(n -> System.out.println(n));
+    // operators.forEach(n -> System.out.println(n));
+
+    String filename = "input.txt";
     // System.out.println("Please enter a filename: ");
     //
     // String filename = sc.next();
     // openFile(filename);
     // sc.close();
-    System.out.println(new Object() {
-      @Override
-      public String toString() {
-        return "Hello, World!!";
-      }
-    });
+    //
+    //
+    Stream<String> streamLines = Files.lines(Paths.get(filename));
+    streamLines.forEach(System.out::println);
+
   }
 }
 
