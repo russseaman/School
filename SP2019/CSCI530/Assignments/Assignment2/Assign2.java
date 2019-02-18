cf91418fc04e00fca0997132819b56f37721f9e2
russseaman <russ_seaman@mac.com>
2019-02-18 10:22:04 -0500

    current working state...need to change how im reading file in...but printing out the operators!

--- SP2019/CSCI530/Assignments/Assignment2/Assign2.java
+++ SP2019/CSCI530/Assignments/Assignment2/Assign2.java
@@ -1,50 +1,96 @@
-import java.io.*;
 import java.util.*;
-import java.util.Spliterator;
+import java.io.*;
 import java.nio.CharBuffer;
 import java.nio.file.*;
+import java.util.*;
+import java.util.Spliterator;
+import java.util.stream.*;
+import java.nio.CharBuffer;
 import java.util.stream.*;
 
 @SuppressWarnings("rawtypes")
-public class Assign2 {
-
-  // public static void keywordCheck(String charAt)
+public class testbed {
 
   public static void stringParse(String filename) throws FileNotFoundException {
+    List<String> keywords = Arrays.asList("int", "double", "String");
+
     List<Character> charList = new ArrayList<Character>();
     PrintStream ps = new PrintStream(System.out);
     Scanner s = null;
+    char ch;
     try {
       s = new Scanner(new BufferedReader(new FileReader(filename)));
       while (s.hasNext()) {
         String str = s.next();
         for (int i = 0; i <= str.length() - 1; i++) {
-          // if (i == 0) {
-          //   System.out.println((i + 1) + "\t" + str.charAt(i));
-          // }
           charList.add(str.charAt(i));
           ps.println(charList);
         }
+
+        // operatorCheck(charList);
       }
+      // ps.println(charList);
+      for (int i = 0; i < charList.size(); i++) {
+        ch = charList.get(i);
+        if (Character.isLetter(ch)) {
+          ps.println(ch + "\tLetter!");
+        } else if (Character.isDigit(ch)) {
+          ps.println(ch + "\tDigit!");
+        } else {
+          switch (ch) {
+          case '=':
+            ps.println("operator: " + ch);
+            break;
+          case '(':
+            ps.println("operator: " + ch);
+            break;
+          case ')':
+            ps.println("operator: " + ch);
+            break;
+          case '+':
+            ps.println("operator: " + ch);
+            break;
+          case '-':
+            ps.println("operator: " + ch);
+            break;
+          case '*':
+            ps.println("operator: " + ch);
+            break;
+          case '/':
+            ps.println("operator: " + ch);
+            break;
+          case ',':
+            ps.println("operator: " + ch);
+            break;
+          case ';':
+            ps.println("operator: " + ch);
+            break;
+          default:
+            ps.println("NOT recognized" + ch);
+          }
+        }
+      }
+
     } finally {
       if (s != null) {
         s.close();
       }
     }
+  }
+
+  public static void charDit() {
 
   }
 
-  public static class keywords {
-    List<String> keywords = Arrays.asList("int", "double", "String");
-  }
-
-  public class operators {
-    List operators = Arrays.asList("=", "(", ")", "+", "-", "*", "/", ",", ";");
-  }
-
-  public class printRes {
-
-  }
+  // public static void operatorCheck(List listCheck) {
+  //   List operators = Arrays.asList("=", "(", ")", "+", "-", "*", "/", ",", ";");
+  //
+  //   for (int i = 0; i <= charList.size(); i++) {
+  //     if (charList.get(i) == "=") {
+  //       return EQUAL;
+  //     }
+  //   }
+  // }
 
   public static void main(String[] args) throws IOException {
     Scanner sc = new Scanner(System.in);
@@ -54,7 +100,5 @@ public class Assign2 {
     // String filename = sc.next();
     stringParse(filename);
     // sc.close();
-
   }
-
 }
--- SP2019/CSCI530/Assignments/Assignment2/testbed.java
+++ SP2019/CSCI530/Assignments/Assignment2/testbed.java
@@ -1,40 +1,104 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 import java.nio.CharBuffer;
 import java.nio.file.*;
+import java.util.*;
+import java.util.Spliterator;
+import java.util.stream.*;
+import java.nio.CharBuffer;
 import java.util.stream.*;
 
+@SuppressWarnings("rawtypes")
 public class testbed {
 
-  public static void main(String[] args) throws IOException {
-    String filename = "input.txt";
+  public static void stringParse(String filename) throws FileNotFoundException {
+    List<String> keywords = Arrays.asList("int", "double", "String");
 
-    Reader r = new FileReader(filename);
+    List<Character> charList = new ArrayList<Character>();
+    PrintStream ps = new PrintStream(System.out);
+    Scanner s = null;
+    char ch;
+    try {
+      s = new Scanner(new BufferedReader(new FileReader(filename)));
+      while (s.hasNext()) {
+        String str = s.next();
+        for (int i = 0; i <= str.length() - 1; i++) {
+          charList.add(str.charAt(i));
+          ps.println(charList);
+        }
 
-    int i = -1;
-    while ((i = r.read()) != -1) {
-      System.out.println((char) i);
+        // operatorCheck(charList);
+      }
+      // ps.println(charList);
+      for (int i = 0; i < charList.size(); i++) {
+        ch = charList.get(i);
+        if (Character.isLetter(ch)) {
+          ps.println(ch + "\tLetter!");
+        } else if (Character.isDigit(ch)) {
+          ps.println(ch + "\tDigit!");
+        } else {
+          switch (ch) {
+          case '=':
+            ps.println("operator: " + ch);
+            break;
+          case '(':
+            ps.println("operator: " + ch);
+            break;
+          case ')':
+            ps.println("operator: " + ch);
+            break;
+          case '+':
+            ps.println("operator: " + ch);
+            break;
+          case '-':
+            ps.println("operator: " + ch);
+            break;
+          case '*':
+            ps.println("operator: " + ch);
+            break;
+          case '/':
+            ps.println("operator: " + ch);
+            break;
+          case ',':
+            ps.println("operator: " + ch);
+            break;
+          case ';':
+            ps.println("operator: " + ch);
+            break;
+          default:
+            ps.println("NOT recognized" + ch);
+          }
+        }
+      }
+
+    } finally {
+      if (s != null) {
+        s.close();
+      }
     }
-    r.close();
   }
-  // public static List<Character> listChars(String str) {
-  //   List<Character> chars = new ArrayList<>();
-  //   for (char ch : str.toCharArray()) {
-  //     chars.add(ch);
-  //   }
-  //   return chars;
-  // }
-  //
-  // @SuppressWarnings("rawtypes")
-  // public static void main(String[] args) throws IOException {
-  //   List keywords = Arrays.asList("int", "double", "String");
+
+  public static void charDit() {
+
+  }
+
+  // public static void operatorCheck(List listCheck) {
   //   List operators = Arrays.asList("=", "(", ")", "+", "-", "*", "/", ",", ";");
-  //   // keywords.forEach(n -> System.out.println(n));
-  //   // operators.forEach(n -> System.out.println(n));
   //
-  //
-  //   Stream<String> streamLines = Files.lines(Paths.get(filename));
-  //   streamLines.forEach(System.out::println);
-  //
-  //   String s = streamLines.stream().map(Object::toString).collect(Collectors.joining(","));
+  //   for (int i = 0; i <= charList.size(); i++) {
+  //     if (charList.get(i) == "=") {
+  //       return EQUAL;
+  //     }
+  //   }
+  // }
+
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+
+    String filename = "input.txt";
+    // System.out.println("Please enter a filename: ");
+    // String filename = sc.next();
+    stringParse(filename);
+    // sc.close();
+  }
 }
