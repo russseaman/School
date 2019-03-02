3534803920fb3d0db131a19f28ac467d171fb48a
russseaman <russ_seaman@mac.com>
2019-03-02 18:21:46 -0500

    trying some changes...stuck :?

--- SP2019/CSCI530/Assignments/Assignment3/userScanner.java
+++ SP2019/CSCI530/Assignments/Assignment3/userScanner.java
@@ -11,21 +11,26 @@ public class userScanner {
     }
 
     public String splitString() {
-        String userIn = userScan();
+        String userIn = "(id + id) * id$";
+        // String userIn = userScan();
         Character chIndex;
-        ArrayList<String> pStr = new ArrayList<String>();
-        for (int i = 0; i < userIn.length(); i++) {
-            chIndex = userIn.charAt(i);
-            if (Character.isLetter(chIndex) && Character.isLetter(chIndex + 1)) {
-                pStr.add(chIndex + "" + userIn.charAt(i + 1));
-                i++;
-            } else if (Character.isWhitespace(chIndex)) {
-            } else {
-                pStr.add(chIndex + "");
-            }
-        }
-        System.out.println(pStr);
-        return pStr.toString();
+
+        Character[] charObjectArray = userIn.chars().mapToObj(c -> (char) c).toArray(Character[]::new);
+        // System.out.println(charObjectArray)÷
+        ArrayList<Character> pStr = new ArrayList<Character>();
+        System.out.println(charObjectArray.toString());
+        // for (int i = 0; i < userIn.length(); i++) {
+        // chIndex = userIn.charAt(i);
+        // if (Character.isLetter(chIndex) && Character.isLetter(chIndex + 1)) {
+        // pStr.add(chIndex + "" + userIn.charAt(i + 1));
+        // i++;
+        // } else if (Character.isWhitespace(chIndex)) {
+        // } else {
+        // pStr.add(chIndex + "");
+        // }
+        // }
+        return charObjectArray.toString();
+        // return ;
 
     }
 }
